08909c4805e240513d90f34337bc09e379d6b854
Gaetano Vallarelli <biroplane@users.noreply.github.com>
2025-02-04 22:28:48 +0100

    Adds components

---
+++ components/content/Heading.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+defineProps<{level:number}>()
+</script>
+<template>
+  <component :is="`h${level}`" class="prose">
+    <slot />
+  </component>
+</template>
---
+++ components/content/Image.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+defineProps<{src:string}>()
+</script>
+
+<template>
+  <img loading="lazy" :src />
+</template>
